d4f8467c231deab72953704b14691abcbb0089ea
SwEikil <gyla.babyla56@gmail.com>
2025-10-30 14:47:41 +0100

    ....

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cathaven.app',
   appName: 'Cat Haven',
-  webDir: 'dist/lonic-project/browser'
+  webDir: 'www'
 };
 
 export default config;
